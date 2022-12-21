1f3f2534133711a5bb99527a0ed2d8359fdcfced
sh0gg_ <davidmine800@gmail.com>
2022-12-21 09:21:20 +0100

    revisar tio que desaparece da fallo

--- PuertasPuertasPuertas/script/script.js
+++ PuertasPuertasPuertas/script/script.js
@@ -1,24 +1,40 @@
 // necesitamos código para que cuando .button:focus
 // .above cambie su visibilidad a hidden
 
-document.querySelector('.button').addEventListener('focus', function() {
-    document.querySelector('.above').style.opacity = 0;
-  });
-  
+//document.querySelector('.button').addEventListener('focus', function() {
+//    document.querySelector('.above').style.opacity = 0;
+//  });
+
 // vale listo, lo has solucionado, pero ahora desaparece para siempre HAHAHAHA
 
-  document.querySelector('.button').addEventListener('blur', function() {
-    document.querySelector('.above').style.opacity = 1;
-  });
+//  document.querySelector('.button').addEventListener('blur', function() {
+//    document.querySelector('.above').style.opacity = 1;
+//  });
 
 // el script para cuando no se abre una puerta
 
 var enlaces = document.querySelectorAll("a");
 var audio = document.getElementById("sonido-puerta");
 
-enlaces.forEach(function(enlace) {
-  enlace.addEventListener("click", function() {
-    audio.play();
-  });
+enlaces.forEach(function (enlace) {
+    enlace.addEventListener("click", function () {
+        audio.play();
+    });
 });
 
+
+// nuevo scrip para manejar above (no funciona)
+
+// Añade un manejador de evento para el evento "focus" del elemento de clase "button"
+document.querySelector('.button').addEventListener('focus', function () {
+
+    // Oculta el elemento de clase "above"
+    document.querySelector('.above').style.opacity = 0;
+});
+
+// Añade un manejador de evento para el evento "focusin" del elemento de clase "button"
+document.querySelector('.button').addEventListener('focusin', function () {
+
+    // Muestra el elemento de clase "above"
+    document.querySelector('.above').style.opacity = 1;
+});

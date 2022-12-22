a9c439bb437f555202100b4ff93f6f82f44b3628
sh0gg_ <davidmine800@gmail.com>
2022-12-22 18:12:22 +0100

    Co-authored-by: porta26 <porta26@users.noreply.github.com>

--- PuertasPuertasPuertas/script/script.js
+++ PuertasPuertasPuertas/script/script.js
@@ -1,34 +1,80 @@
 const above = document.querySelector('.above');
 const button = document.querySelector('.button');
 
-button.addEventListener('focus', function () { 
-    above.classList.add('.hidden');
-}); 
-
-
-
-var enlaces = document.querySelectorAll("a");
-var audio = document.getElementById("sonido-puerta");
-
-enlaces.forEach(function (enlace) {
-    enlace.addEventListener("click", function () {
-        audio.play();
-    });
+button.addEventListener('focus', function () {
+  above.classList.add('.hidden');
 });
 
-/* Comandos para tocarle los huevos a Porta
-window.onload = function() {
-    alert("Nuestra página utiliza millones de cookies que te infectará el PC con software de criptominería, no puedes no aceptarlas. Lo sentimos.");
+
+// sonido para todos los enlaces
+var enlaces = document.querySelectorAll("a");
+var noAbre = document.getElementById("sonido-puerta");
+
+enlaces.forEach(function (enlace) {
+  enlace.addEventListener("click", function () {
+    noAbre.play();
+  });
+});
+
+var chuerk = document.getElementById("chuerk");
+var holy = document.getElementById("holy");
+var mine = document.getElementById("mine");
+var rick = document.getElementById("rick");
+
+var puertachuerk = document.getElementById("puertachuerk");
+var puertaholy = document.getElementById("puertaholy");
+var puertamine = document.getElementById("puertamine");
+var puertarick = document.getElementById("puertarick");
+
+puertachuerk.forEach(function (puertac) {
+  puertac.addEventListener('click', function () {
+    chuerk.play();
+  })
+});
+
+puertaholx.forEach(function (puertah) {
+  puertah.addEventListener('click', function () {
+    holy.play();
+  })
+});
+
+puertamine.forEach(function (puertam) {
+  puertam.addEventListener('click', function () {
+    mine.play();
+  })
+});
+
+puertarick.forEach(function (puertar) {
+  puertar.addEventListener('click', function () {
+    rick.play();
+  })
+});
+
+enlaces.addEventListener('click', function () {
+  if (enlaces.style.animationPlayState === 'paused') {
+    enlaces.style.animationPlayState = 'running';
+  } else {
+    enlaces.style.animationPlayState = 'paused';
+  }
+});
+
+window.onload = function () {
+  alert("Nuestra página utiliza millones de cookies que te infectará el PC con software de criptominería, no puedes no aceptarlas. Lo sentimos.");
 };
 
 function showPrompt() {
-    var result = prompt("Para continuar en la página, por favor acepta nuestra política de privacidad y cookies");
-    if (result !== "acepto") {
-      window.location.href = "http://www.google.com";
-    }
+  var result = prompt("Para continuar en la página, por favor acepta nuestra política de privacidad y cookies. Escribe acepto:");
+  if (result !== "acepto") {
+    window.location.href = "http://www.google.com";
   }
-  
-  window.onload = function() {
+}
+
+let hasClicked = false
+
+window.document.addEventListener('click', function () {
+  if (!hasClicked) {
     showPrompt();
+    hasClicked = true;
   }
-  */
+})
+
